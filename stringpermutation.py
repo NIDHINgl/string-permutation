c427f2c76d198a50a8ddab9ad9b48aebe0c4ed08
NIDHINgl <38777673+NIDHINgl@users.noreply.github.com>
2018-10-19 11:23:49 +0530

    Add files via upload

---
+++ stringpermutation.py
@@ -0,0 +1,26 @@
+a = ['a','e','i','o','u']
+for f in range(len(a)):
+	for g in range(len(a)):
+		str1 = a[f] + a[g]
+		print(str1)
+if str1 != 0:
+	for i in range(len(a)):
+	    for j in range(len(a)):
+		    for k in range(len(a)):
+		        str2 =  a[i] + a[j] + a[k]
+		        print(str2)
+if str2 != 0:
+	for b in range(len(a)):
+		for c in range(len(a)):
+			for d in range(len(a)):
+				for e in range(len(a)):
+					str3 = a[b] + a[c] + a[d] + a[e]
+					print(str3)
+if str3 != 0:
+	for l in range(len(a)):
+		for m in range(len(a)):
+			for n in range(len(a)):
+				for o in range(len(a)):
+					for p in range(len(a)):
+						str4 = a[l] + a[m] + a[n] + a[o] + a[p]
+						print(str4)
